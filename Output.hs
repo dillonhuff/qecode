a2dff02b97d09385dbdf722d49f3c0a30ee9f6ba
Dillon Huff <dillonhuff@gmail.com>
2017-05-17 15:14:55 -0700

    Working on primitive parser

--- Output.hs
+++ Output.hs
@@ -1,14 +1,37 @@
 module Output where
 
+import Text.Parsec.Combinator
+import Text.ParserCombinators.Parsec.Combinator
+import Text.Parsec (ParseError)
+import Text.ParserCombinators.Parsec
+import Text.Parsec.String
+
 data Expr = Expr deriving (Eq, Ord, Show)
 
 data Comparator = EQ | NEQ | GEQ | LEQ deriving (Eq, Ord, Show)
 
-data Formula =
-  And [Formula]
-  Or [Formula]
+data Formula = 
+  And [Formula] |
+  Or [Formula] |
   Term Comparator Expr Expr deriving (Eq, Ord, Show)
 
 
+pFormula = do
+  char '('
+  a <- many1 (char 'a')
+  char ')'
+  return (Or [])
+
+  -- do
+  -- char '('
+  -- a <- many1 "abcdefghijklmnopqrstuvwxyz123456789-+=<*"
+  -- char ')'
+  -- return (Or [])
+
+parseFormula :: String -> Either ParseError Formula
+parseFormula input = parse pFormula "(unknown)" input
+
 main :: IO ()
-main = putStrLn "hello"
+main = do
+  a <- readFile "val_example.txt"
+  putStrLn a
