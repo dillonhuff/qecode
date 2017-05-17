d35fe28f88762cef9d7e522725d297c743f1334c
Dillon Huff <dillonhuff@gmail.com>
2017-05-17 15:45:14 -0700

    Now trying to create a parser. Way harder than I was expecting

---
+++ ExParse.hs
@@ -0,0 +1,39 @@
+module ExParse where
+
+import Text.Parsec
+import Text.Parsec.Expr
+
+
+--import Text.Parsec.Token (parens)
+
+-- parens :: (Monad m) => a -> ParsecT String u m a
+-- parens p = do
+--     char '('
+--     e <- p
+--     char ')'
+--     return e
+
+-- reservedOp :: (Monad m) => String -> ParsecT String u m ()
+-- reservedOp s = do
+--     string s
+--     notFollowedBy letter
+
+-- natural = fmap read $ many1 digit
+
+-- expr    = buildExpressionParser table term
+
+-- term    =  parens expr 
+--            <|> natural
+--            <?> "simple expression"
+
+-- table   = [ [prefix "-" negate, prefix "+" id ]
+--           , [postfix "++" (+1), postfix "--" (subtract 1)]  
+--           , [binary "*" (*) AssocLeft, binary "/" (div) AssocLeft ]
+--           , [binary "+" (+) AssocLeft, binary "-" (-)   AssocLeft ]
+--           ]
+
+-- binary  name fun assoc = Infix (do{ reservedOp name; return fun }) assoc
+-- prefix  name fun       = Prefix (do{ reservedOp name; return fun })
+-- postfix name fun       = Postfix (do{ reservedOp name; return fun })
+
+-- res = runParser expr () "expr" "1--"
--- Output.hs
+++ Output.hs
@@ -1,6 +1,8 @@
 module Output where
 
 import Text.Parsec.Combinator
+import Text.Parsec.Expr
+import Text.Parsec.Token
 import Text.ParserCombinators.Parsec.Combinator
 import Text.Parsec (ParseError)
 import Text.ParserCombinators.Parsec
@@ -8,19 +10,35 @@ import Text.Parsec.String
 
 data Expr = Expr deriving (Eq, Ord, Show)
 
-data Comparator = EQ | NEQ | GEQ | LEQ deriving (Eq, Ord, Show)
+data Comparator = EQL | NEQ | GEQ | LEQ deriving (Eq, Ord, Show)
 
 data Formula = 
   And [Formula] |
   Or [Formula] |
   Term Comparator Expr Expr deriving (Eq, Ord, Show)
 
+-- parseTerm = do
+--   spaces
+--   char '('
+  
+--   char ')'
+--   return (Term EQL Expr Expr)
 
-pFormula = do
-  char '('
-  a <- many1 (char 'a')
-  char ')'
-  return (Or [])
+-- parseClause str = return $ Or []
+--   -- char '('
+--   -- --fms <- sepBy pFormula (symbol str)
+--   -- char ')'
+--   -- case str of
+--   --  "||" -> return $ Or []
+--   --  "&&" -> return $ And []
+
+-- pFormula = do
+--   char '('
+--   newline
+--   spaces
+--   a <- (parseClause "||") <|> (parseClause "&&") <|> parseTerm
+--   char ')'
+--   return a
 
   -- do
   -- char '('
@@ -28,10 +46,30 @@ pFormula = do
   -- char ')'
   -- return (Or [])
 
-parseFormula :: String -> Either ParseError Formula
-parseFormula input = parse pFormula "(unknown)" input
+-- parseFormula :: String -> Either ParseError Formula
+-- parseFormula input = parse pFormula "(unknown)" input
 
+expr    = buildExpressionParser table term
+         <?> "expression"
+
+term    =  parens expr 
+          <|> natural
+          <?> "simple expression"
+
+table   = [ [prefix "-" negate, prefix "+" id ]
+            , [postfix "++" (+1)]
+            , [binary "*" (*) AssocLeft, binary "/" (div) AssocLeft ]
+            , [binary "+" (+) AssocLeft, binary "-" (-)   AssocLeft ]
+            ]
+         
+binary  name fun assoc = Infix (do{ reservedOp name; return fun }) assoc
+prefix  name fun       = Prefix (do{ reservedOp name; return fun })
+postfix name fun       = Postfix (do{ reservedOp name; return fun })
+                          
 main :: IO ()
 main = do
   a <- readFile "val_example.txt"
   putStrLn a
+  -- case parseFormula a of
+  --  Left e -> putStrLn $ show e
+  --  Right f -> putStrLn $ show f
