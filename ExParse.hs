6b282791ec160762c6f8d6f21b2bb512fb46799b
Dillon Huff <dillonhuff@gmail.com>
2017-05-18 16:43:55 -0700

    Cube sphere intersection runs

--- ExParse.hs
+++ ExParse.hs
@@ -1,10 +1,10 @@
 module ExParse where
 
+import CppOut
 import Formula
 import Parser
 
 import Data.List
-import Data.Char
 import System.Process
 import Text.Parsec
 import Text.Parsec.Expr
@@ -19,53 +19,6 @@ replace a b s@(x:xs) = if isPrefixOf a s
 
 replace _ _ [] = []
 
-showCppBinopNoParens str a b = (showCpp a) ++ " " ++ str ++ " " ++ (showCpp b)
-showCppBinop str a b = "( " ++ (showCppBinopNoParens str a b) ++ " )"
-
-cppVarList :: [String] -> String
-cppVarList [] = ""
-cppVarList (a:[]) = "const double " ++ a
-cppVarList (a:as) = "const double " ++ a ++ ", " ++ (cppVarList as)
-
-varNamesBinop a b = (varNames a) ++ (varNames b)
-
-varNames :: Arith -> [String]
-varNames (Times a b) = varNamesBinop a b
-varNames (Plus a b) = varNamesBinop a b
-varNames (Minus a b) = varNamesBinop a b
-varNames (GEQ a b) = varNamesBinop a b
-varNames (LEQ a b) = varNamesBinop a b
-varNames (EQL a b) = varNamesBinop a b
-varNames (NEQ a b) = varNamesBinop a b
-varNames (Pow a b) = varNamesBinop a b
-varNames (Or a b) = varNamesBinop a b
-varNames (And a b) = varNamesBinop a b
-varNames (Var i) = if all (\c -> (isDigit c) || (c == '.')) i then [] else [i]
-varNames (Num i) = []
-varNames e = error $ show e
-
-varNamesUnique expr = nub $ varNames expr
-
-showCppDecl :: Arith -> String
-showCppDecl expr = "bool test( " ++ (cppVarList $ varNamesUnique $ expr) ++ " )"
-
-showCpp :: Arith -> String
-showCpp (Or a b) = showCppBinop "||" a b
-showCpp (And a b) = showCppBinop "&&" a b
-showCpp (NEQ a b) = showCppBinop "!=" a b
-showCpp (GEQ a b) = showCppBinop ">=" a b
-showCpp (LEQ a b) = showCppBinop "<=" a b
-showCpp (EQL a b) = showCppBinop "==" a b
-showCpp (Times a b) = showCppBinopNoParens "*" a b
-showCpp (Minus a b) = showCppBinopNoParens "-" a b
-showCpp (Plus a b) = showCppBinopNoParens "+" a b
-showCpp (Pow a b) = "pow( " ++ (showCpp a) ++ ", " ++ (showCpp b) ++ " )"
-showCpp (Var r) = r
-
-showCpp a = error $ show a
-
-cppTestString expr = "#include<cmath>\n\n" ++ (showCppDecl expr) ++ " { return " ++
-                     (showCpp expr) ++ "; }"
 
 preprocessFormulaString a =
   replace ". " "" $ replace "**" "^" (filter (\c -> c /= '\n') a)
@@ -77,7 +30,7 @@ preprocessFormulaString a =
 qePrefix = "load redlog;\nrlset ofsf;\nin \"shape_formulas.red\";\n"
 qeSuffix = "val := rlqe phi;\nin \"output.red\"$\nwriteFormula(val);"
 
-qeFormula = "ex( x, ex( y, inCircleFormula(a, b, r) and inSquareFormula(a, b, l) and (l > 0) and (r > 0) ) )"
+qeFormula = "ex( x, ex( y, ex( z, inSphereFormula(a, b, c, r) and inCubeFormula(f, g, h, l) and (l > 0) and (r > 0) ) ) )"
 
 qeString = qePrefix ++ "\nphi := " ++ qeFormula ++ ";\nval := rlqe phi;\n" ++ qeSuffix
 
