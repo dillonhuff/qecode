78244302334d5827e7cb6d091ee691455997e345
Dillon Huff <dillonhuff@gmail.com>
2017-05-17 17:31:43 -0700

    Building an arithmetic parser

--- ExParse.hs
+++ ExParse.hs
@@ -5,35 +5,44 @@ import Text.Parsec.Expr
 
 --import Text.Parsec.Token (parens)
 
+data Arith =
+  Times Arith Arith |
+  Plus Arith Arith |
+  Minus Arith Arith |
+  Neg Arith deriving (Eq, Ord, Show)
+  
+parens :: (Monad m) => ParsecT String u m a -> ParsecT String u m a
+parens p = do
+    char '('
+    e <- p
+    char ')'
+    return e
 
--- parens :: (Monad m) => a -> ParsecT String u m a
--- parens p = do
---     char '('
---     e <- p
---     char ')'
---     return e
+reservedOp :: (Monad m) => String -> ParsecT String u m ()
+reservedOp s = do
+    string s
+    notFollowedBy letter
 
--- reservedOp :: (Monad m) => String -> ParsecT String u m ()
--- reservedOp s = do
---     string s
---     notFollowedBy letter
+natural = fmap read $ many1 digit
 
--- natural = fmap read $ many1 digit
+expr    = buildExpressionParser table term
 
--- expr    = buildExpressionParser table term
+term    =  parens expr 
+           <|> natural
+           <?> "simple expression"
 
--- term    =  parens expr 
---            <|> natural
---            <?> "simple expression"
+table   = [ [prefix "-" negate, prefix "+" id ]
+          , [postfix "++" (+1), postfix "--" (subtract 1)]
+          , [binary "**" (*) AssocLeft]
+          , [binary "*" (*) AssocLeft, binary "/" (div) AssocLeft ]
+          , [binary "+" (+) AssocLeft, binary "-" (-)   AssocLeft ]
+          ]
 
--- table   = [ [prefix "-" negate, prefix "+" id ]
---           , [postfix "++" (+1), postfix "--" (subtract 1)]  
---           , [binary "*" (*) AssocLeft, binary "/" (div) AssocLeft ]
---           , [binary "+" (+) AssocLeft, binary "-" (-)   AssocLeft ]
---           ]
+binary  name fun assoc = Infix (do{ reservedOp name; return fun }) assoc
+prefix  name fun       = Prefix (do{ reservedOp name; return fun })
+postfix name fun       = Postfix (do{ reservedOp name; return fun })
 
--- binary  name fun assoc = Infix (do{ reservedOp name; return fun }) assoc
--- prefix  name fun       = Prefix (do{ reservedOp name; return fun })
--- postfix name fun       = Postfix (do{ reservedOp name; return fun })
+res = runParser expr () "expr" "(3**1)"
 
--- res = runParser expr () "expr" "1--"
+main :: IO ()
+main = putStrLn $ show res
