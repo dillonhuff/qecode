6ca8a15fc6e101831c53bff32d9d78e10247b56d
Dillon Huff <dillonhuff@gmail.com>
2017-05-17 17:54:52 -0700

    For some reason the parser cannot handle variables after an operator if no spaces separate them, eg "2+a" fails

--- ExParse.hs
+++ ExParse.hs
@@ -3,19 +3,29 @@ module ExParse where
 import Text.Parsec
 import Text.Parsec.Expr
 
---import Text.Parsec.Token (parens)
-
 data Arith =
   Times Arith Arith |
   Plus Arith Arith |
   Minus Arith Arith |
-  Neg Arith deriving (Eq, Ord, Show)
+  Pow Arith Arith |
+  NEQ Arith Arith |
+  GEQ Arith Arith |
+  LEQ Arith Arith |
+  EQL Arith Arith |
+  And Arith Arith |
+  Or Arith Arith |
+  Neg Arith |
+  Num Int |
+  Var String deriving (Eq, Ord, Show)
   
 parens :: (Monad m) => ParsecT String u m a -> ParsecT String u m a
 parens p = do
     char '('
+    spaces
     e <- p
+    spaces
     char ')'
+    spaces
     return e
 
 reservedOp :: (Monad m) => String -> ParsecT String u m ()
@@ -23,26 +33,38 @@ reservedOp s = do
     string s
     notFollowedBy letter
 
-natural = fmap read $ many1 digit
+natural = do
+  spaces
+  fmap (Num . read) $ many1 digit
+
+variable = do
+  spaces
+  chars <- many1 alphaNum
+  spaces
+  return $ Var chars
 
 expr    = buildExpressionParser table term
 
-term    =  parens expr 
+term    =  parens expr
+           <|> variable
            <|> natural
            <?> "simple expression"
 
-table   = [ [prefix "-" negate, prefix "+" id ]
-          , [postfix "++" (+1), postfix "--" (subtract 1)]
-          , [binary "**" (*) AssocLeft]
-          , [binary "*" (*) AssocLeft, binary "/" (div) AssocLeft ]
-          , [binary "+" (+) AssocLeft, binary "-" (-)   AssocLeft ]
+table   = [ [prefix "-" Neg, prefix "+" id ]
+          , [binary "**" Pow AssocLeft]
+          , [binary "*" Times AssocLeft] --, binary "/" (div) AssocLeft ]
+          , [binary "+" Plus AssocLeft, binary "-" Minus AssocLeft ]
+          , [binary "!=" NEQ AssocLeft, binary ">=" GEQ AssocLeft]
+          , [binary "&&" And AssocLeft]
           ]
 
 binary  name fun assoc = Infix (do{ reservedOp name; return fun }) assoc
 prefix  name fun       = Prefix (do{ reservedOp name; return fun })
 postfix name fun       = Postfix (do{ reservedOp name; return fun })
 
-res = runParser expr () "expr" "(3**1)"
+res = runParser expr () "expr" "(3**1)+a"
 
 main :: IO ()
-main = putStrLn $ show res
+main = do
+  a <- readFile "val_example_no_newlines.txt"
+  putStrLn $ show $ runParser expr () "expr" a
