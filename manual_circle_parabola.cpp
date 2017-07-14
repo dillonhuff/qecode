82e65b1b073870fbd75055c8efc31eab032230f7
Dillon Huff <dillonhuff@gmail.com>
2017-07-14 15:15:58 -0700

    Now manually building test polynomial

--- manual_circle_parabola.cpp
+++ manual_circle_parabola.cpp
@@ -2,7 +2,17 @@
 
 using namespace ralg;
 
+// Circle parabola without y formula:
+// ( a**2-2.0*a*x+b**2-2.0*b*c*x**2-2.0*b*d*x-2.0*b*l+c**2*x**4+2.0*c*d*x**3+2.0*c*l*x**2+d**2*x**2+2.0*d*l*x+l**2-r**2+x**2 <= 0.0 )
 int main() {
-  monomial x(1, {1, 0}, 2);
+  // Variable numbering:  a, b, c, d, l, r, x
+  monomial a(1, {1, 0, 0, 0, 0, 0, 0}, 7);
+  monomial b(1, {0, 1, 0, 0, 0, 0, 0}, 7);
+  monomial c(1, {0, 0, 1, 0, 0, 0, 0}, 7);
+  monomial d(1, {0, 0, 0, 1, 0, 0, 0}, 7);
+  monomial l(1, {0, 0, 0, 0, 1, 0, 0}, 7);
+  monomial r(1, {0, 0, 0, 0, 0, 1, 0}, 7);
+  monomial x(1, {0, 0, 0, 0, 0, 0, 1}, 7);
+
   return 0;
 }
