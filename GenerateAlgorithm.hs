87bb64a9cc2ba01c326d8af09f931f041c3fa8eb
Dillon Huff <dillonhuff@gmail.com>
2017-07-15 13:15:23 -0700

    Added test_points_from_roots utility function

--- GenerateAlgorithm.hs
+++ GenerateAlgorithm.hs
@@ -4,7 +4,9 @@ import Formula
 
 within_eps_def = "bool within_eps(const double a, const double b, const double eps) {\n\treturn fabs(a - b) < eps;\n}"
 
-algoPrefixCpp eps = "#include <ralg/root_counting.h>\nusing namespace std;\nusing namespace ralg;\n\n" ++ within_eps_def
+test_points_from_roots_def = "std::vector<rational> test_points_from_roots(const std::vector<rational>& roots) {\nif (roots.size() == 0) { return {};\n } vector<rational> sorted_roots = roots;\n sort(begin(sorted_roots), end(sorted_roots));\n rational one(1);\n rational two(2);\n rational neg_inf = sorted_roots.front() - 1;\n rational pos_inf = sorted_roots.back() + 1;\n vector<rational> test_points;\n test_points.push_back(neg_inf);\n for (int i = 0; i < sorted_roots.size(); i++) {\ntest_points.push_back(sorted_roots[i]);\n if (i < sorted_roots.size() - 1) { rational mid = (sorted_roots[i] + sorted_roots[i + 1]) / two;\n test_points.push_back(mid);\n } } test_points.push_back(pos_inf);\n return test_points;\n }"
+  
+algoPrefixCpp eps = "#include <ralg/root_counting.h>\n\nusing namespace std;\nusing namespace ralg;\n\n#define EPSILON " ++ show eps ++ "\n\n" ++ within_eps_def ++ "\n\n" ++ test_points_from_roots_def
 
 algorithmTextCpp =
   let eps = 0.0001 in
