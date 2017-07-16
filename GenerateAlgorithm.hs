a13bcb31a55a16c95bd9bcb2e5aa34b618a179ec
Dillon Huff <dillonhuff@gmail.com>
2017-07-15 19:33:48 -0700

    Added variable ordering and separated out the distinguished variable in algorithmTextCpp

--- GenerateAlgorithm.hs
+++ GenerateAlgorithm.hs
@@ -8,7 +8,7 @@ within_eps_def = "bool within_eps(const double a, const double b, const double e
 
 test_points_from_roots_def = "std::vector<rational> test_points_from_roots(const std::vector<rational>& roots) {\nif (roots.size() == 0) { return {};\n } vector<rational> sorted_roots = roots;\n sort(begin(sorted_roots), end(sorted_roots));\n rational one(1);\n rational two(2);\n rational neg_inf = sorted_roots.front() - 1;\n rational pos_inf = sorted_roots.back() + 1;\n vector<rational> test_points;\n test_points.push_back(neg_inf);\n for (int i = 0; i < sorted_roots.size(); i++) {\ntest_points.push_back(sorted_roots[i]);\n if (i < sorted_roots.size() - 1) { rational mid = (sorted_roots[i] + sorted_roots[i + 1]) / two;\n test_points.push_back(mid);\n } } test_points.push_back(pos_inf);\n return test_points;\n }"
   
-algoPrefixCpp eps fm = "#include <ralg/root_counting.h>\n\nusing namespace std;\nusing namespace ralg;\n\n#define EPSILON " ++ show eps ++ "\n\n" ++ within_eps_def ++ "\n\n" ++ test_points_from_roots_def ++ "\n\n" ++ (formulaFunctionCpp fm)
+algoPrefixCpp eps var vars fm = "#include <ralg/root_counting.h>\n\nusing namespace std;\nusing namespace ralg;\n\n#define EPSILON " ++ show eps ++ "\n\n" ++ within_eps_def ++ "\n\n" ++ test_points_from_roots_def ++ "\n\n" ++ (formulaFunctionCpp var vars fm)
 
 fmCppBinop op a b = "( " ++ (formulaCpp a) ++ " " ++ op ++ " " ++ (formulaCpp b) ++ " )"
 
@@ -17,24 +17,28 @@ formulaCpp (EQL a b) = "within_eps( " ++ (commaList $ map formulaCpp [a, b]) ++ 
 formulaCpp (Plus a b) = fmCppBinop "+" a b
 formulaCpp (Times a b) = fmCppBinop "*" a b
 formulaCpp (Minus a b) = fmCppBinop "-" a b
+formulaCpp (Num x) = show x
 
 varStrings (Var x) = [x]
 varStrings (EQL a b) = (varStrings a) ++ (varStrings b)
 varStrings (Plus a b) = (varStrings a) ++ (varStrings b)
 varStrings (Minus a b) = (varStrings a) ++ (varStrings b)
 varStrings (Times a b) = (varStrings a) ++ (varStrings b)
+varStrings (Num _) = []
 
 varStrs fm = L.nub $ varStrings fm
 
-formulaFunctionCpp fm =
-  "bool formula(" ++ commaList (L.map (\s -> "const double " ++ s) $ varStrs fm) ++ ") {\n" ++ "\treturn " ++ (formulaCpp fm) ++ ";\n}\n"
+formulaFunctionCpp var@(Var s) vars fm =
+  let varStr = commaList $ L.map (\s -> "const double " ++ s) (vars ++ [s]) in
+   "bool formula(" ++ varStr ++ ") {\n" ++ "\treturn " ++ (formulaCpp fm) ++ ";\n}\n"
 
 algorithmTextCpp :: Arith -> Arith -> String
-algorithmTextCpp var fm =
-  let eps = 0.0001 in
-   algoPrefixCpp eps fm
+algorithmTextCpp var@(Var s) fm =
+  let eps = 0.0001
+      vars = L.delete s $ L.sort $ varStrs fm in
+   algoPrefixCpp eps var vars fm
 
-fm = EQL (Minus (Minus (Plus (Times (Var "a") (Var "x")) (Var "b")) (Times (Var "c") (Var "x"))) (Var "d")) (Var "0")
+fm = EQL (Minus (Minus (Plus (Times (Var "a") (Var "x")) (Var "b")) (Times (Var "c") (Var "x"))) (Var "d")) (Num 0.0)
 
 main :: IO ()
 main = putStrLn $ algorithmTextCpp (Var "x") fm
