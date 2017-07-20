730a9ed14f3df6a750b2d23fb8f7218e3cb69f60
Dillon Huff <dillonhuff@gmail.com>
2017-07-20 11:13:36 -0700

    There is definitely a numerical issue with using polynomials. Line box intersection shows that there is disagreement between the full QE (line_x_rectangle) and only 1 quantifier (line_rectangle)

--- autogen/line_rectangle_tests.cpp
+++ autogen/line_rectangle_tests.cpp
@@ -1,8 +1,27 @@
 #include "line_rectangle.h"
-#include "line_x_rectangle.h"
+
+#define CATCH_CONFIG_MAIN
 
 #include "catch.hpp"
 
 
-TEST_CASE() {
+TEST_CASE("Line rectangle pure") {
+  double line_slope = -1.0;
+  double line_intercept = 0.0;
+
+  double rectangle_x_min = -0.0000001;
+  double rectangle_y_min = -0.0000001;
+
+  double rectangle_x_max = 1.0;
+  double rectangle_y_max = 1.0;
+
+  bool it = line_rectangle(line_slope,
+			   line_intercept,
+			   rectangle_x_min,
+			   rectangle_y_min,
+			   rectangle_x_max,
+			   rectangle_y_max);
+
+  REQUIRE(it);
+
 }
