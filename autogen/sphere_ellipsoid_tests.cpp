1e177a411dc3f061e92b53b3a6e99ae9d5e04036
Dillon Huff <dillonhuff@gmail.com>
2017-07-21 11:14:17 -0700

    Some numerical issues with large numbers of polynomials

--- autogen/sphere_ellipsoid_tests.cpp
+++ autogen/sphere_ellipsoid_tests.cpp
@@ -78,3 +78,78 @@ TEST_CASE("X-Y plane and just below ellipsoid") {
   REQUIRE(intersect);
 
 }
+
+TEST_CASE("X-Y plane on ellipsoid") {
+  // Plane variables
+  double a = 0;
+  double b = 0;
+  double c = 1;
+  double d = 1.0;
+
+  //Ellipsoid variables
+
+  // Offsets
+  double f = 0;
+  double g = 0;
+  double h = 0;
+
+  // Major axis lengths
+  double l = 3; // x
+  double m = 2; // y
+  double n = 1; // z
+
+  bool intersect = sphere_ellipsoid(  a,  b,  c,  d,  f,  g,  h,  l,  m,  n );
+  
+  REQUIRE(intersect);
+
+}
+
+TEST_CASE("X-Y plane just above ellipsoid") {
+  // Plane variables
+  double a = 0;
+  double b = 0;
+  double c = 1;
+  double d = 1.01;
+
+  //Ellipsoid variables
+
+  // Offsets
+  double f = 0;
+  double g = 0;
+  double h = 0;
+
+  // Major axis lengths
+  double l = 3; // x
+  double m = 2; // y
+  double n = 1; // z
+
+  bool intersect = sphere_ellipsoid(  a,  b,  c,  d,  f,  g,  h,  l,  m,  n );
+  
+  REQUIRE(!intersect);
+
+}
+
+TEST_CASE("X-Y plane 0.1 above ellipsoid") {
+  // Plane variables
+  double a = 0;
+  double b = 0;
+  double c = 1;
+  double d = 1.1;
+
+  //Ellipsoid variables
+
+  // Offsets
+  double f = 0;
+  double g = 0;
+  double h = 0;
+
+  // Major axis lengths
+  double l = 3; // x
+  double m = 2; // y
+  double n = 1; // z
+
+  bool intersect = sphere_ellipsoid(  a,  b,  c,  d,  f,  g,  h,  l,  m,  n );
+  
+  REQUIRE(!intersect);
+
+}
