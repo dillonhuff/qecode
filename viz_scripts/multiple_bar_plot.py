4f9dc14a76c82f76ca6549c95425be272c54b920
Dillon Huff <dillonhuff@gmail.com>
2017-07-21 13:56:04 -0700

    Reducing root optimization

--- viz_scripts/multiple_bar_plot.py
+++ viz_scripts/multiple_bar_plot.py
@@ -2,9 +2,9 @@ import numpy as np
 import matplotlib.pyplot as plt
  
 # data to plot
-n_groups = 4
-means_frank = (90, 55, 40, 65)
-means_guido = (85, 62, 54, 20)
+n_groups = 2
+opt_scores = (0.19, 0.3)
+no_opt_scores = (0.32, 0.2)
  
 # create plot
 fig, ax = plt.subplots()
@@ -12,12 +12,12 @@ index = np.arange(n_groups)
 bar_width = 0.35
 opacity = 0.8
  
-rects1 = plt.bar(index, means_frank, bar_width,
+rects1 = plt.bar(index, no_opt_scores, bar_width,
                  alpha=opacity,
                  color='b',
                  label='No analytic root optimization')
  
-rects2 = plt.bar(index + bar_width, means_guido, bar_width,
+rects2 = plt.bar(index + bar_width, opt_scores, bar_width,
                  alpha=opacity,
                  color='r',
                  label='Analytic root optimization')
@@ -25,7 +25,7 @@ rects2 = plt.bar(index + bar_width, means_guido, bar_width,
 plt.xlabel('Intersection Test')
 plt.ylabel('Benchmark Time')
 plt.title('Effect of Analytic Root Finding Optimization')
-plt.xticks(index + bar_width, ('Circle-Ellipse', 'Circle-Circle', 'Line-Line', 'Ellipsoid-Plane'))
+plt.xticks(index + bar_width, ('Circle-Ellipse', 'Ellipsoid-Plane'))
 plt.legend()
  
 plt.tight_layout()
