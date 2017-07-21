4bb37521498a7f69195a9c6d973ebc2483a1f457
Dillon Huff <dillonhuff@gmail.com>
2017-07-21 11:10:15 -0700

    sphere-ellipsoid passes some basic tests

---
+++ autogen/sphere_ellipsoid_tests.cpp
@@ -0,0 +1,80 @@
+#include "sphere_ellipsoid.h"
+
+#define CATCH_CONFIG_MAIN
+
+#include "catch.hpp"
+
+TEST_CASE("X-Y plane and ellipsoid") {
+  // Plane variables
+  double a = 0;
+  double b = 0;
+  double c = 1;
+  double d = 0;
+
+  //Ellipsoid variables
+
+  // Offsets
+  double f = 0;
+  double g = 0;
+  double h = 0;
+
+  // Major axis lengths
+  double l = 3; // x
+  double m = 2; // y
+  double n = 1; // z
+
+  bool intersect = sphere_ellipsoid(  a,  b,  c,  d,  f,  g,  h,  l,  m,  n );
+  
+  REQUIRE(intersect);
+
+}
+
+TEST_CASE("X-Y plane and above ellipsoid") {
+  // Plane variables
+  double a = 0;
+  double b = 0;
+  double c = 1;
+  double d = 7;
+
+  //Ellipsoid variables
+
+  // Offsets
+  double f = 0;
+  double g = 0;
+  double h = 0;
+
+  // Major axis lengths
+  double l = 3; // x
+  double m = 2; // y
+  double n = 1; // z
+
+  bool intersect = sphere_ellipsoid(  a,  b,  c,  d,  f,  g,  h,  l,  m,  n );
+  
+  REQUIRE(!intersect);
+
+}
+
+TEST_CASE("X-Y plane and just below ellipsoid") {
+  // Plane variables
+  double a = 0;
+  double b = 0;
+  double c = 1;
+  double d = 0.9;
+
+  //Ellipsoid variables
+
+  // Offsets
+  double f = 0;
+  double g = 0;
+  double h = 0;
+
+  // Major axis lengths
+  double l = 3; // x
+  double m = 2; // y
+  double n = 1; // z
+
+  bool intersect = sphere_ellipsoid(  a,  b,  c,  d,  f,  g,  h,  l,  m,  n );
+  
+  REQUIRE(intersect);
+
+}
