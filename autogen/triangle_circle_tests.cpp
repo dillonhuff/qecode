87c1280308ee6051798f11de614cd35db18882f0
Dillon Huff <dillonhuff@gmail.com>
2017-07-22 16:07:39 -0700

    A do not overlap test case also passes

--- autogen/triangle_circle_tests.cpp
+++ autogen/triangle_circle_tests.cpp
@@ -8,7 +8,7 @@
 
 using namespace std;
 
-TEST_CASE("Overlap case 1") {
+TEST_CASE("Triangle and circle do overlap") {
 
   double a = 0.1;
   double b = 0.3;
@@ -27,5 +27,29 @@ TEST_CASE("Overlap case 1") {
 
   cout << "Intersect = " << intersect << endl;
 
+
+  REQUIRE(intersect);
+}
+
+TEST_CASE("Triangle and circle dont overlap") {
+
+  double a = 0.1;
+  double b = 0.3;
+  double r = 3;;
+
+  double v1x = 8 + -0.2;
+  double v1y = 8 + -0.1;
+
+  double v2x = 20 + 1.0;
+  double v2y = 16 + 2.3;
+
+  double v3x = 10 + 1.0;
+  double v3y = 10 + 0.2;
+
+  bool intersect = triangle_circle(  a,  b,  r,  v1x,  v1y,  v2x,  v2y,  v3x, v3y );
+
+  cout << "Intersect = " << intersect << endl;
+
+  REQUIRE(!intersect);
   
 }
