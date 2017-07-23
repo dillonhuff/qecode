6715289f4f053b52e85b1d1465764eb1ce4f0bfa
Dillon Huff <dillonhuff@gmail.com>
2017-07-23 15:16:14 -0700

    Value at root is not close to zero for the 1 root case

--- autogen/cubic_roots.cpp
+++ autogen/cubic_roots.cpp
@@ -37,23 +37,37 @@ vector<double> cubic_roots(const double a,
 
   cout << "p = " << p << endl;
 
-  assert(p < 0);
+  if (p < 0) {
 
-  double A = 2.0*sqrt(-p / 3.0);
-
-  double phi = acos((3.0*q) / (A*p));
-
-  double B = -b / (3.0*a);
-
-  double r1 = A*cos((1/3.0)*phi) + B;
-  double r2 = A*cos((1/3.0)*(phi + 2*M_PI)) + B;
-  double r3 = A*cos((1/3.0)*(phi + 4*M_PI)) + B;
-
-  cout << "r1 = " << r1 << endl;
-  cout << "r2 = " << r2 << endl;
-  cout << "r3 = " << r3 << endl;
+    double A = 2.0*sqrt(-p / 3.0);
+    double phi = acos((3.0*q) / (A*p));
+    double B = -b / (3.0*a);
   
-  return {r1, r2, r3};
+
+    double r1 = A*cos((1/3.0)*phi) + B;
+    double r2 = A*cos((1/3.0)*(phi + 2*M_PI)) + B;
+    double r3 = A*cos((1/3.0)*(phi + 4*M_PI)) + B;
+
+    cout << "r1 = " << r1 << endl;
+    cout << "r2 = " << r2 << endl;
+    cout << "r3 = " << r3 << endl;
+  
+    return {r1, r2, r3};
+  } else if (p > 0) {
+    double A = 2*sqrt(p/3.0);
+    double phi = asinh((3.0*q) / (A*p));
+    double r1 = -A * sinh(phi / 3.0);
+
+    cout << "r1 = " << r1 << endl;
+    //double B = -b / (3.0*a);
+
+    return {r1};
+    
+  } else {
+    assert(false);
+  }
+
+  
 }
 
 double evaluate_cubic(const double a,
@@ -63,15 +77,37 @@ double evaluate_cubic(const double a,
 		      const double x) {
   assert(!within_eps(a, 0.0));
 
-  return 0.0;
+  return a*pow(x, 3.0) + b*pow(x, 2.0) + c*pow(x, 1.0) + d;
 }
 
-TEST_CASE("x^3 - 6x^2 + 11x - 6") {
+TEST_CASE("x^3 - 6x^2 + 11x - 6, 3 roots") {
   double a = 1;
   double b = -6;
   double c = 11;
   double d = -6;
 
   REQUIRE(cubic_roots(a, b, c, d).size() == 3);
+}
 
+TEST_CASE("10x^3 + 5^2 + 1x - 1, 1 real") {
+  double a = 10;
+  double b = 5;
+  double c = 1;
+  double d = -1;
+
+  auto roots = cubic_roots(a, b, c, d);
+  double r = roots[0];
+
+  cout << "value at root = " << evaluate_cubic(a, b, c, d, r) << endl;
+
+  REQUIRE(roots.size() == 1);
+}
+
+TEST_CASE("x^3 - 3x^2 + 3x - 1, 1 coincident") {
+  double a = 1;
+  double b = -3;
+  double c = 3;
+  double d = -1;
+
+  //REQUIRE(cubic_roots(a, b, c, d).size() == 1);
 }
